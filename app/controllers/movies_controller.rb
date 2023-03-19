29b383e7e012ccc3a845a139a4fa0b6617e364ec
Marcus <marcus.kariuki@student.moringaschool.com>
2023-03-19 21:55:41 +0300

    assignment completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  
+  end
+
+
   def index
     movies = Movie.all
     render json: movies
